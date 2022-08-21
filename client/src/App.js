9c7cc2e6c0db7421bdf56113159ed1991c38bb10
AndrewsSouza <andrews.souza@outlook.com.br>
2022-08-21 02:15:31 -0300

    beginning with kubernetes

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React version 2 (Before Kubernetes)
+            Learn React version 2 (After Kubernetes)
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
